07d7f7d3da5242f4849f6638cec55b59dc300a74
user <hunchan12@gmail.com>
2018-11-13 18:23:43 +0900

    블록체인투표 1113

---
+++ app/static/index.js
@@ -0,0 +1,23 @@
+web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"));
+abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]');
+VotingContext = web3.eth.contract(abi)
+contractInstance = VotingContext.at('0x74E13e39403A520BC5A9774102a72fb6CD3aFEc5');
+candidate = {"0xcFeF13EaD94953eeA8E7CA5cB9b3C0eD689b5c7c":"candidate-1",
+"0x6E83458A4E8573bAdbd82D1DdC756BEc4f835eBe":"candidate-2",
+"0xcAf65B6F67fF3DCDd12e06D006E2C1a6acf1F5e6":"candidate-3"};
+
+function voteForCandidate(){
+
+}
+
+$(document).ready(function(){
+    candidateNames = Object.keys(candidate);
+
+    for (var i = 0; i < candidateNames.length; i++){
+        let name = candidateNames[i];
+        console.log(name);
+        let val = contractInstance.totalVotesFor.call(name).toString();
+        $('#' + candidate[name]).html(val);
+    }
+
+});
